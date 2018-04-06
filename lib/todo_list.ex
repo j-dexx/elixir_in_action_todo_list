59d13f6dc595e11a2f8cf17a76c28950e8e7a517
James Doyley <jdoyley@gmail.com>
2018-04-06 09:07:17 +0100

    Switch to auto generated ids and add update and delete functionality

--- lib/todo_list.ex
+++ lib/todo_list.ex
@@ -1,18 +1,50 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
 
-  def new(), do: %{}
+  def new(), do: %TodoList{}
 
-  def add_entry(todo_list, entry) do
-    Map.update(
-      todo_list,
-      entry.date,
-      [entry],
-      fn entries -> [entry | entries] end
+  def add_entry(todo_list = %TodoList{}, entry) do
+    # Set the id for the entry
+    entry = Map.put(entry, :id, todo_list.auto_id)
+
+    # Add the entry
+    new_entries = Map.put(
+      todo_list.entries,
+      todo_list.auto_id,
+      entry
     )
+
+    # Increment the auto_id field
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
   end
 
-  def entries(todo_list, date) do
-    Map.get(todo_list, date, [])
+  def update_entry(todo_list = %TodoList{}, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        old_entry_id = old_entry.id
+        new_entry = %{id: ^old_entry_id} = updater_fun.(old_entry) # Ensure id has not changed by updater lambda
+        new_entries = Map.put(todo_list.entries, new_entry.id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  # Alternative update interface - passing a new entry to replace an old one
+  def update_entry(todo_list = %TodoList{}, %{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def delete_entry(todo_list = %TodoList{}, entry_id) do
+    new_entries = Map.delete(todo_list.entries, entry_id)
+    %TodoList{todo_list | entries: new_entries}
+  end
+
+  # Use stream then enum so both transformations happen in single pass through the collection
+  def entries(todo_list = %TodoList{}, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
   end
 end
--- test/todo_list_test.exs
+++ test/todo_list_test.exs
@@ -2,8 +2,8 @@ defmodule TodoListTest do
   use ExUnit.Case
   doctest TodoList
 
-  test "new returns an empty map" do
-    assert TodoList.new == %{}
+  test "new returns an empty struct" do
+    assert TodoList.new == %TodoList{}
   end
 
   test "simple todo list" do
@@ -11,7 +11,34 @@ defmodule TodoListTest do
       |> TodoList.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
       |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
 
-    assert [%{date: ~D[2018-01-01], title: "Dinner"}] == TodoList.entries(todo_list, ~D[2018-01-01])
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] == TodoList.entries(todo_list, ~D[2018-01-01])
     assert [] == TodoList.entries(todo_list, ~D[2018-01-03])
+  end
+
+  test "todo updating entry" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
+      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Meeting"})
+
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Updated"}] ==
+             todo_list
+             |> TodoList.update_entry(1, fn entry -> %{entry | title: "Updated"} end)
+             |> TodoList.entries(~D[2018-01-01])
+
+  end
+
+  test "deleting entry" do
+    todo_list =
+      TodoList.new()
+      |> TodoList.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
+      |> TodoList.add_entry(%{date: ~D[2018-01-02], title: "Meeting"})
+
+    assert [] ==
+             todo_list
+             |> TodoList.delete_entry(1)
+             |> TodoList.entries(~D[2018-01-01])
   end
 end
