7e505ad4d042a026b2e150bf6f1a174e4add8eb1
James Doyley <jdoyley@gmail.com>
2018-05-04 08:58:39 +0100

    Make db server registered

--- test/todo_server_test.exs
+++ test/todo_server_test.exs
@@ -1,39 +1,50 @@
 defmodule TodoServerTest do
   use ExUnit.Case
 
-  test "adding entries" do
-    pid = TodoServer.start()
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
+  setup do
+    on_exit fn ->
+      Process.whereis(:todo_server)
+      |> Process.exit(:kill)
+    end
+  end
 
-    assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] == TodoServer.entries(pid, ~D[2018-01-01])
+  test "adding entries" do
+    TodoServer.start()
+    :timer.sleep(100)
+    TodoServer.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
+    TodoServer.add_entry(%{date: ~D[2018-01-02], title: "Meeting"})
+
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Dinner"}] == TodoServer.entries(~D[2018-01-01])
   end
 
   test "updating entry using updater function" do
-    pid = TodoServer.start()
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Dentist"})
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-02], title: "Meeting"})
+    TodoServer.start()
+    :timer.sleep(100)
+    TodoServer.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.add_entry(%{date: ~D[2018-01-02], title: "Dentist"})
+    TodoServer.add_entry(%{date: ~D[2018-01-02], title: "Meeting"})
 
-    TodoServer.update_entry(pid, 1, fn entry -> %{entry | title: "Updated"} end)
+    TodoServer.update_entry(1, fn entry -> %{entry | title: "Updated"} end)
 
-    assert [%{date: ~D[2018-01-01], id: 1, title: "Updated"}] == TodoServer.entries(pid, ~D[2018-01-01])
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Updated"}] == TodoServer.entries(~D[2018-01-01])
   end
 
   test "updating entry using new entry" do
-    pid = TodoServer.start()
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
-    TodoServer.update_entry(pid, %{date: ~D[2018-01-01], id: 1, title: "Updated"})
+    TodoServer.start()
+    :timer.sleep(100)
+    TodoServer.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.update_entry(%{date: ~D[2018-01-01], id: 1, title: "Updated"})
 
-    assert [%{date: ~D[2018-01-01], id: 1, title: "Updated"}] == TodoServer.entries(pid, ~D[2018-01-01])
+    assert [%{date: ~D[2018-01-01], id: 1, title: "Updated"}] == TodoServer.entries(~D[2018-01-01])
   end
 
   test "deleting entry using id" do
-    pid = TodoServer.start()
-    TodoServer.add_entry(pid, %{date: ~D[2018-01-01], title: "Dinner"})
-    TodoServer.delete_entry(pid, 1)
+    TodoServer.start()
+    :timer.sleep(100)
+    TodoServer.add_entry(%{date: ~D[2018-01-01], title: "Dinner"})
+    TodoServer.delete_entry(1)
 
-    assert [] == TodoServer.entries(pid, ~D[2018-01-01])
+    assert [] == TodoServer.entries(~D[2018-01-01])
   end
 end
