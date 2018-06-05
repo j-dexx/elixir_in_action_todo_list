162470b7dd2a470bfe3a1259f328e77beb73d11f
James Doyley <jdoyley@gmail.com>
2018-06-05 18:21:06 +0100

    Make the cache supervised

--- test/todo/cache_test.exs
+++ test/todo/cache_test.exs
@@ -14,19 +14,24 @@ defmodule Todo.CacheTest do
   end
 
   test "server_process" do
-    {:ok, cache} = Todo.Cache.start()
-    bob_pid = Todo.Cache.server_process(cache, "bob")
+    {:ok, supervisor_pid} = Todo.System.start_link()
+    bob_pid = Todo.Cache.server_process("bob")
 
-    assert bob_pid != Todo.Cache.server_process(cache, "alice")
-    assert bob_pid == Todo.Cache.server_process(cache, "bob")
+    assert bob_pid != Todo.Cache.server_process("alice")
+    assert bob_pid == Todo.Cache.server_process("bob")
+
+    Supervisor.stop(supervisor_pid)
   end
 
   test "to-do operations" do
-    {:ok, cache} = Todo.Cache.start()
-    alice = Todo.Cache.server_process(cache, "alice")
+    {:ok, supervisor_pid} = Todo.System.start_link()
+
+    alice = Todo.Cache.server_process("alice")
     Todo.Server.add_entry(alice, %{date: ~D[2018-12-19], title: "Dentist"})
     entries = Todo.Server.entries(alice, ~D[2018-12-19])
 
     assert [%{date: ~D[2018-12-19], title: "Dentist"}] = entries
+
+    Supervisor.stop(supervisor_pid)
   end
 end
